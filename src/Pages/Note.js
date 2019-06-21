9890144154f97f80e61c577a89df5ab96d0e64a0
Alexandre78R <alexandre.renard98@gmail.com>
2019-06-21 12:18:31 +0200

    Ajoute de react-draggable pour les note et mise en place ok

--- src/Pages/Note.js
+++ src/Pages/Note.js
@@ -12,9 +12,17 @@ import NavBar from '../Composent/NavBar'
 import SideBar from '../Composent/SideBar';
 // import Footer from '../Composent/Footer';
 import { Link } from "react-router-dom";
+import Draggable from 'react-draggable';
 
 class Note extends React.Component {
- 
+  
+  constructor() {
+    super();
+    this.state = {
+    };
+    
+  }
+
   render() {
     return (
       <div id="page-top">
@@ -26,9 +34,10 @@ class Note extends React.Component {
               <Container className="container-fluid">
               <Button className = "note-button" color="success"><Link className="textBtnTableMessage" to={'#'}>Ajouté une note</Link></Button>
                <Row className="note-page">
+               <Draggable>
                <div className="note">
                   <div className="note-topic org">
-                      My Project
+                      My Project1
                       <span className="close"><i className="far fa-times-circle"></i></span>
                       <span className="close"><i className="far fa-edit"></i></span>
                   </div>
@@ -36,9 +45,11 @@ class Note extends React.Component {
                     <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
                   </div>
               </div>
-              <div className="note">
+              </Draggable>
+              <Draggable>
+               <div className="note">
                   <div className="note-topic org">
-                      My Project
+                      My Project1
                       <span className="close"><i className="far fa-times-circle"></i></span>
                       <span className="close"><i className="far fa-edit"></i></span>
                   </div>
@@ -46,9 +57,11 @@ class Note extends React.Component {
                     <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
                   </div>
               </div>
-              <div className="note">
+              </Draggable>
+              <Draggable>
+               <div className="note">
                   <div className="note-topic org">
-                      My Project
+                      My Project1
                       <span className="close"><i className="far fa-times-circle"></i></span>
                       <span className="close"><i className="far fa-edit"></i></span>
                   </div>
@@ -56,107 +69,7 @@ class Note extends React.Component {
                     <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
                   </div>
               </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              <div className="note">
-                  <div className="note-topic org">
-                      My Project
-                      <span className="close"><i className="far fa-times-circle"></i></span>
-                      <span className="close"><i className="far fa-edit"></i></span>
-                  </div>
-                  <div className="note-body">
-                    <p> Ipsum incididunt esse ex amet velit deserunt ea id et velit qui. Sit amet excepteur aute aliquip nulla ea reprehenderit ullamco sit. Ea exercitation cupidatat tempor aliqua ex sunt aliquip laboris occaecat velit occaecat non pariatur. Laborum cillum est ut esse enim excepteur eiusmod nulla nostrud excepteur labore nisi dolore. Ut sit excepteur nulla consequat magna proident consectetur amet magna id velit aute. Id amet eiusmod enim magna sint consectetur reprehenderit nisi ad et. Voluptate exercitation eiusmod qui nulla quis est sunt id consequat minim. Ut reprehenderit duis in nisi eiusmod non duis aliqua anim nulla qui. Ipsum quis velit amet laboris irure aliqua quis aliqua do veniam non ut laborum minim.</p>
-                  </div>
-              </div>
-              
+              </Draggable>
                </Row>
              </Container>
              {/* <Footer/> */}
